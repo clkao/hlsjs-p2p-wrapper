9a2b412f6c9ce29cfe9c567976785a52d3c07cb2
Axel Delmas <axel@streamroot.io>
2016-01-19 15:25:13 +0100

    Implement getSegmentsList and unit test suite

--- test/media-map.js
+++ test/media-map.js
@@ -59,4 +59,85 @@ describe("MediaMap",() => {
       mediaMap.getSegmentTime.bind(mediaMap, segmentCoord).should.throw("Called getSegmentTime on a level that was not parsed yet (or whose index didn't exist)");
     });
   });
+  describe("getSegmentsList", function() {
+    it("Should return list of segments in timerange (timerange included in segment index)", function() {
+      let hlsMock = new HlsMock(3, false, 1);
+      let mediaMap = new MediaMap(hlsMock);
+      let trackCoord = new TrackCoord({
+        level: 1
+      });
+      let expectedSegmentList = [ 
+        new SegmentCoord({sn: 37, trackCoord}),
+        new SegmentCoord({sn: 38, trackCoord}),
+        new SegmentCoord({sn: 39, trackCoord}),
+      ];
+      mediaMap.getSegmentsList(trackCoord, 365, 33).should.be.eql(expectedSegmentList);
+    });
+    it("Should return list of segments in timerange (left intersection)", function() {
+      let hlsMock = new HlsMock(3, false, 1);
+      let mediaMap = new MediaMap(hlsMock);
+      let trackCoord = new TrackCoord({
+        level: 1
+      });
+      let expectedSegmentList = [ 
+        new SegmentCoord({sn: 25, trackCoord}),
+        new SegmentCoord({sn: 26, trackCoord}),
+        new SegmentCoord({sn: 27, trackCoord}),
+        new SegmentCoord({sn: 28, trackCoord}),
+      ];
+      mediaMap.getSegmentsList(trackCoord, 10, 275).should.be.eql(expectedSegmentList);
+    });
+    it("Should return list of segments in timerange (right intersection)", function() {
+      let hlsMock = new HlsMock(3, false, 1);
+      let mediaMap = new MediaMap(hlsMock);
+      let trackCoord = new TrackCoord({
+        level: 1
+      });
+      let expectedSegmentList = [ 
+        new SegmentCoord({sn: 198, trackCoord}),
+        new SegmentCoord({sn: 199, trackCoord}),
+      ];
+      mediaMap.getSegmentsList(trackCoord, 1975, 3000).should.be.eql(expectedSegmentList);
+    });
+    it("Should return list of segments in timerange (timerange includes segment index)", function() {
+      let hlsMock = new HlsMock(3, false, 1);
+      let mediaMap = new MediaMap(hlsMock);
+      let trackCoord = new TrackCoord({
+        level: 1
+      });
+      let expectedSegmentList = [];
+
+      for (var f=25; f<200; f++) {
+        expectedSegmentList.push(new SegmentCoord({
+          sn: f,
+          trackCoord
+        }));
+      }
+
+      mediaMap.getSegmentsList(trackCoord, 240, 2100).should.be.eql(expectedSegmentList);
+    });
+    it("Should return an empty array if no segment is found in that timerange", function() {
+      let hlsMock = new HlsMock(3, false, 1);
+      let mediaMap = new MediaMap(hlsMock);
+      let trackCoord = new TrackCoord({
+        level: 1
+      });
+      let expectedSegmentList = [];
+
+      mediaMap.getSegmentsList(trackCoord, 2100, 3000).should.be.eql(expectedSegmentList);
+    });
+    it("Should throw if called on a level that is not parsed", function() {
+      let hlsMock = new HlsMock(3, false, 0);
+      let mediaMap = new MediaMap(hlsMock);
+      let trackCoord = new TrackCoord({
+        level: 1
+      });
+      let expectedSegmentList = [ 
+        new SegmentCoord({sn: 37, trackCoord}),
+        new SegmentCoord({sn: 38, trackCoord}),
+        new SegmentCoord({sn: 39, trackCoord}),
+      ];
+      mediaMap.getSegmentsList.bind(mediaMap, trackCoord, 365, 33).should.throw("Called getSegmentsList on a level that was not parsed yet (or whose index didn't exist)");
+    });
+  });
 });
