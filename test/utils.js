57e4667f6fe60fd6a10d86f5e4a645e1abcce24a
Axel Delmas <axel@streamroot.io>
2016-03-29 15:42:51 +0200

    Add failing test case: if contentId is specified but is a falsy value, we want to throw if it's not a string

--- test/utils.js
+++ test/utils.js
@@ -10,4 +10,9 @@ describe('Content ID format helper method: cleanManifestString', function() {
     const contentId = "My-id:is%so?kewl";
     formatContentId({ contentUrl, contentId }).should.equal(contentId);
   });
+  it('should compute the hash the same way as the backend for special urls', function() {
+    const contentUrl = "http://cdn.rtmp.webverbosity.com:1935/dvr/chardikla_12HRDVR/playlist.m3u8?dvr";
+    const contentId = 0;
+    formatContentId.bind(null, { contentUrl, contentId }).should.throw("Content identifier needs to be a string");
+  });
 });
