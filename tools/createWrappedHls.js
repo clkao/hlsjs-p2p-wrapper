92a81aa5784d590e3c9eb5c0f8cc258164bddac5
Stephan <stephan@soundcloud.com>
2016-05-02 16:48:55 +0200

    Don't override `loader` in config but `fLoader`. We only want to use the P2PLoader for fragment resources, not for playlists and keys. This fixes issues where the P2PCache would be poluted by encryption keys and we would return their data instead of media segment data to the player later.

--- tools/createWrappedHls.js
+++ tools/createWrappedHls.js
@@ -4,7 +4,7 @@ import HlsjsWrapper from "../lib/streamroot-wrapper";
 function createWrappedHls(StreamrootDownloader, Hls, p2pConfig, hlsConfig = {}, customId=undefined){
     var hlsjsWrapper = new HlsjsWrapper(StreamrootDownloader);
 
-    hlsConfig.loader = hlsjsWrapper.P2PLoader;
+    hlsConfig.fLoader = hlsjsWrapper.P2PLoader;
     hlsConfig.xhrSetup = hlsjsWrapper.setRange;
 
     //Set buffer configuration params, unless they're specified
