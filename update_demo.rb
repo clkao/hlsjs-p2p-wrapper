6d622e2a456219d448c954a8e3ba260414818b98
Stephan <stephan@soundcloud.com>
2016-05-30 17:39:46 +0200

    add npm hook for update demo

--- update_demo.rb
+++ update_demo.rb
@@ -4,7 +4,7 @@ require 'json'
 
 # Updates the Hls.js demo based on the supported version
 
-CURRENT_FOLDER = File.expand_path File.dirname(__FILE__)
+CURRENT_FOLDER = File.expand_path(File.dirname(__FILE__))
 
 PACKAGE = File.read(File.join(CURRENT_FOLDER, 'package.json'))
 PACKAGE_HASH = JSON.parse(PACKAGE)
@@ -13,11 +13,11 @@ VERSION = PACKAGE_HASH['dependencies']['hls.js']
 FILENAME = "v#{VERSION}.tar.gz"
 DEMO_DIR = File.join(CURRENT_FOLDER, "demo-hls.js")
 
-Dir.chdir CURRENT_FOLDER
+Dir.chdir(CURRENT_FOLDER)
 
 `mkdir -p #{DEMO_DIR}`
 
-Dir.chdir DEMO_DIR
+Dir.chdir(DEMO_DIR)
 
 `rm -Rf *`
 `wget https://github.com/dailymotion/hls.js/archive/#{FILENAME}`
@@ -41,5 +41,3 @@ INDEX = File.read(DEMO_INDEX)
 NEW_INDEX = INDEX.gsub(OLD_CONSTRUCTOR, NEW_CONSTRUCTOR).gsub(OLD_SCRIPT, NEW_SCRIPT)
 
 File.open(DEMO_INDEX, "w") {|file| file.puts NEW_INDEX}
-
-
