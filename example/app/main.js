7c9893e6259db5265ff052820afefe47183b8da2
Stephan Hesse <tchakabam@gmail.com>
2016-06-21 15:05:29 +0200

    add additional app example code

--- example/app/main.js
+++ example/app/main.js
@@ -1,8 +1,10 @@
 import HlsjsWrapper from '../../dist/wrapper/hlsjs-p2p-wrapper';
 import Hls from 'hls.js';
 
-window.StartApp = function(hlsjsConfig, p2pConfig) {
+window.CreatePlayer = function(hlsjsConfig, p2pConfig) {
 
-	(new HlsjsP2PWrapper(Hls)).createPlayer(hlsjsConfig || {}, p2pConfig);
+	return (new HlsjsP2PWrapper(Hls)).createPlayer(hlsjsConfig || {}, p2pConfig);
 
 }
+
+window.Hls = Hls;
