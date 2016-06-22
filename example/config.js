38d8ae30bdf0f0d43954a2ae1703f1b18a2465e4
Stephan Hesse <tchakabam@gmail.com>
2016-06-23 00:33:36 +0200

    add common window bootstrap file to examples

---
+++ example/config.js
@@ -0,0 +1,17 @@
+window._DEBUG_ = true;
+window._TEST_ = false;
+window._MOBILE_ = false;
+window._ENVIRONMENT_ = "development";
+window.streamrootConfig = {
+    p2pConfig:{
+        streamrootKey: "ry-v7xuywnt",
+        debug: true,
+        contentId: null
+    },
+    hlsjsConfig: {
+    	debug: true
+    },
+    contentUrl: 'http://www.streambox.fr/playlists/test_001/stream.m3u8'
+}
+
+window.previousTotalCDN = 0;
